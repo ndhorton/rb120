c7f648bd7138936863079ded9bfefb1d4f1d73cd
Nicholas Horton <nicholashorton7@protonmail.com>
2023-11-26 17:31:19 +0000

    Add RPS YAML file

--- lesson_3/scratch.rb
+++ lesson_3/scratch.rb
@@ -1,11 +1,24 @@
-class SpoonException < Exception
-  def say_spoon
-    puts "spoon"
-  end
-end
+require 'psych'
 
-begin
-  raise SpoonException if 'spoon' == 'spoon'
-rescue SpoonException => spoon
-  spoon.say_spoon
-end
+TEXTDATA = Psych.load_file("#{__dir__}/yamltest.yml")
+
+text = TEXTDATA['english']
+
+# input = nil
+# puts text['move_prompt']
+# loop do
+#   valid_input = false
+#   input = gets.chomp.strip
+#   break if text['moves'].include?(input)
+#   puts text['invalid_move']
+# end
+
+# input = nil
+# puts "enter (y)es or (n)o"
+# loop do
+#   input = gets.chomp.strip
+#   break if text['yesno'].include?(input)
+#   puts "Invalid. Y or n"
+# end
+
+puts text['rules']
